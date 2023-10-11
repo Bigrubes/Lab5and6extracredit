945e6da40e892208ae112dc02cfcf9f7b78510d2
Reuben Ramos <rsramos23@gmail.com>
2023-10-11 14:52:21 -0700

    lab06 completed

--- js/lab06ec.js
+++ js/lab06ec.js
@@ -6,15 +6,22 @@ let years;
 
 // COLLECT VALUES FROM THE USER
 investment = parseFloat(prompt('Enter investment amount as xxxx.xx'));
-rate = parseFloat(prompt('Enter interest rate as xx.x'));
+rate = parseFloat(prompt('Enter interest rate as x.x'));
 years = parseInt(prompt('Enter the number of years you want to invest for'));
+
+if(!isNaN(investment ) && !isNaN(rate) && rate>=0 && rate<=6 && !isNaN(years) && rate>=1 && rate<=30){
+
+
 
 // CALCULATE FUTURE VALUE
 futureValue = investment;
 for (let i = 0; i < years; i++) {
     futureValue = futureValue + (futureValue * rate / 100);
 }
-
+}
+else{
+        alert('One of the entries was invalid. Please try again');
+    }
 // DISPLAY RESULT
 document.write(`Investment amount: $${investment.toFixed(2)}<br>`);
 document.write(`Interest rate: ${rate.toFixed(2)}%<br>`);
